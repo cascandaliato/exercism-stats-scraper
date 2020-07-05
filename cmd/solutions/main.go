9088141526e1c67b9b9fc321e5b1548050dd0e0e
Carmelo Scandaliato <carmelo.scandaliato@iongroup.com>
2020-07-05 17:33:47 +0200

    first commit

---
+++ cmd/solutions/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"regexp"
+
+	"github.com/apex/gateway"
+)
+
+type solutions struct {
+	Total string `json:"total"`
+}
+
+func main() {
+	port := flag.Int("port", -1, "specify a port to use http rather than AWS Lambda")
+	flag.Parse()
+
+	listener := gateway.ListenAndServe
+	portStr := ":3030"
+	if *port != -1 {
+		listener = http.ListenAndServe
+		portStr = fmt.Sprintf(":%d", *port)
+	}
+
+	http.HandleFunc("/", solutionsJSON)
+
+	log.Fatal(listener(portStr, nil))
+}
+
+func solutionsJSON(w http.ResponseWriter, r *http.Request) {
+	res, err := http.Get("https://exercism.io/profiles/casca")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	robots, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	re := regexp.MustCompile(`Showing (\d+) solutions`)
+	matches := re.FindSubmatch(robots)
+
+	w.Header().Set("Content-Type", "application/json")
+	s := solutions{string(matches[1])}
+	json.NewEncoder(w).Encode(s)
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/casca/exercism-stats-scraper
+
+go 1.14
+
+require (
+	github.com/apex/gateway v1.1.1
+	github.com/aws/aws-lambda-go v1.17.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/tj/assert v0.0.3 // indirect
+)
